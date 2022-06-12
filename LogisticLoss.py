ae8f3768cb2fd83af5931ef9c7d07782136e33ff
GreatSnoopyMe <umlty@umich.edu>
2022-06-12 16:40:00 -0400

    Origin Programs

---
+++ LogisticLoss.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+import os
+import torch.nn.functional as F
+import json
+import numpy as np
+
+
+class BaseModule(nn.Module):
+
+	def __init__(self):
+		super(BaseModule, self).__init__()
+		self.zero_const = nn.Parameter(torch.Tensor([0]))
+		self.zero_const.requires_grad = False
+		self.pi_const = nn.Parameter(torch.Tensor([3.14159265358979323846]))
+		self.pi_const.requires_grad = False
+
+	def load_checkpoint(self, path):
+		self.load_state_dict(torch.load(os.path.join(path)))
+		self.eval()
+
+	def save_checkpoint(self, path):
+		torch.save(self.state_dict(), path)
+
+	def load_parameters(self, path):
+		f = open(path, "r")
+		parameters = json.loads(f.read())
+		f.close()
+		for i in parameters:
+			parameters[i] = torch.Tensor(parameters[i])
+		self.load_state_dict(parameters, strict = False)
+		self.eval()
+
+	def save_parameters(self, path):
+		f = open(path, "w")
+		f.write(json.dumps(self.get_parameters("list")))
+		f.close()
+
+	def get_parameters(self, mode = "numpy", param_dict = None):
+		all_param_dict = self.state_dict()
+		if param_dict == None:
+			param_dict = all_param_dict.keys()
+		res = {}
+		for param in param_dict:
+			if mode == "numpy":
+				res[param] = all_param_dict[param].cpu().numpy()
+			elif mode == "list":
+				res[param] = all_param_dict[param].cpu().numpy().tolist()
+			else:
+				res[param] = all_param_dict[param]
+		return res
+
+	def set_parameters(self, parameters):
+		for i in parameters:
+			parameters[i] = torch.Tensor(parameters[i])
+		self.load_state_dict(parameters, strict = False)
+		self.eval()
+
+class Loss(BaseModule):
+
+	def __init__(self):
+		super(Loss, self).__init__()
+
+class LogisticLoss(Loss):
+	def __init__(self, adv_temperature = None):
+		super(LogisticLoss, self).__init__()
+		if adv_temperature != None:
+			self.adv_temperature = nn.Parameter(torch.Tensor([adv_temperature]))
+			self.adv_temperature.requires_grad = False
+			self.adv_flag = True
+		else:
+			self.adv_flag = False
+	
+	def get_weights(self, n_score):
+		return F.softmax(n_score * self.adv_temperature, dim = -1).detach()
+
+	def forward(self, p_score, n_score):
+		if self.adv_flag:
+			return (torch.log(1 + torch.exp(p_score)).mean() + 
+			(self.get_weights(n_score) * torch.log(1 + torch.exp(-n_score))).sum(dim = -1).mean()) / 2
+		else:
+			return torch.log(1 + torch.exp(p_score)).mean() + torch.log(1 + torch.exp(-n_score)).mean()
+
+	def predict(self, p_score, n_score):
+		score = self.forward(p_score, n_score)
+		return score.cpu().data.numpy()
